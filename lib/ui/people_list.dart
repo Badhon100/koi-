6b72c8fcce5a959cacc04e66e902bd96de5d4f60
Badhon100 <badhonx102@gmail.com>
2023-05-10 12:19:22 +0600

    design people list and create login register file

--- lib/ui/people_list.dart
+++ lib/ui/people_list.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class PeopleList extends StatelessWidget {
+class PeopleList extends StatefulWidget {
   const PeopleList({super.key});
 
   @override
+  State<PeopleList> createState() => _PeopleListState();
+}
+
+class _PeopleListState extends State<PeopleList> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top:8.0, left: 20, right: 20, bottom: 8),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              color: Colors.white10,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                     Text("Badhon Ahmed",
+                      style: TextStyle(
+                        color: (index %2 == 0)?Colors.teal : Colors.redAccent,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Container(
+                      height: 20, width: 20,
+                      decoration: BoxDecoration(
+                        color: (index %2 == 0)?Colors.teal : Colors.redAccent,
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+        ),
+      ),
+    );
   }
 }
